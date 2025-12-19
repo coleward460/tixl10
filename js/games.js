97b2071a9844f1219f98eef6851794d1ccda01e2
Cole <ward_cole1@icloud.com>
2025-12-19 17:47:50 -0500

    fixed paths

--- js/games.js
+++ js/games.js
@@ -2,22 +2,22 @@ const topGamesContainer = document.getElementById("topGames");
 const allGamesContainer = document.getElementById("allGames");
 
 const games = [
-  { name: "Bloxorz", img: "img/bloxorz.png", link: "bloxorz" },
-  { name: "Cat Ninja", img: "img/cat-ninja.png", link: "cat-ninja" },
-  { name: "Fireboy & Watergirl", img: "img/fireboy&watergirl.png", link: "fireboy-and-watergirl" },
-  { name: "Raft Wars", img: "img/raftwars.png", link: "raftwars" },
-  { name: "Raft Wars 2", img: "img/raftwars2.png", link: "raftwars2" },
-  { name: "Run", img: "img/run.png", link: "run" },
-  { name: "Run 2", img: "img/run2.png", link: "run2" },
-  { name: "Run 3", img: "img/run3.png", link: "run3" },
-  { name: "Sports Heads 🏀", img: "img/sportsheadsbasketball.png", link: "sports-heads-basketball" },
-  { name: "Sports Heads ⚽", img: "img/sportsheadsfootball.png", link: "sports-heads-ftbl" },
-  { name: "Sports Heads 🏒", img: "img/sportsheadshockey.png", link: "sports-heads-ice-hockey" },
-  { name: "Sports Heads 🎾", img: "img/sportsheadstennis.png", link: "sports-heads-tennis" },
-  { name: "Sports Heads 🏐", img: "img/sportsheadsvolleyball.png", link: "sports-heads-volleyball" },
-  { name: "Vex", img: "img/vex.png", link: "vex" },
-  { name: "Vex 2", img: "img/vex2.png", link: "vex2" },
-  { name: "Vex 3", img: "img/vex3.png", link: "vex3" },
+  { name: "Bloxorz", location: "bloxorz" },
+  { name: "Cat Ninja", location: "cat-ninja" },
+  { name: "Fireboy & Watergirl", location: "fireboy-and-watergirl" },
+  { name: "Raft Wars", location: "raftwars" },
+  { name: "Raft Wars 2", location: "raftwars2" },
+  { name: "Run", location: "run" },
+  { name: "Run 2", location: "run2" },
+  { name: "Run 3", location: "run3" },
+  { name: "Sports Heads 🏀", location: "sports-heads-basketball" },
+  { name: "Sports Heads ⚽", location: "sports-heads-ftbl" },
+  { name: "Sports Heads 🏒", location: "sports-heads-ice-hockey" },
+  { name: "Sports Heads 🎾", location: "sports-heads-tennis" },
+  { name: "Sports Heads 🏐", location: "sports-heads-volleyball" },
+  { name: "Vex", location: "vex" },
+  { name: "Vex 2", location: "vex2" },
+  { name: "Vex 3", location: "vex3" },
 ];
 
 const topGames = ["Raft Wars", "Run", "Vex 3", "Fireboy & Watergirl"];
@@ -28,11 +28,11 @@ function createCard(game) {
   card.style.cursor = "pointer";
 
   card.addEventListener("click", () => {
-    window.location.href = `game/${game.link}.html`;
+    window.location.href = `./game/${game.location}/index.html`;
   });
 
   const img = document.createElement("img");
-  img.src = game.img;
+  img.src = `./game/${game.location}/cover.png`;
 
   const title = document.createElement("h4");
   title.textContent = game.name;
